f16d5eb0589a4332b2bcbe4c815675b24da2a8e5
Egor <egorant1.ru@mail.ru>
2019-05-05 17:48:56 +0300

    Update Game.cs

--- GameOfEconomy/MainLogic/Game.cs
+++ GameOfEconomy/MainLogic/Game.cs
@@ -34,17 +34,47 @@ namespace GameOfEconomy.MainLogic
         {
             endVars = new List<EEndVar>
             {
-                new EEndVar { Name = "End.Var1", LongName = "Какая то хуйня" },
-                new EEndVar { Name = "End.Var2", LongName = "Какая то хуйня" },
-                new EEndVar { Name = "End.Var3", LongName = "Какая то хуйня" },
-                new EEndVar { Name = "End.Var4", LongName = "Какая то хуйня4" },
+                new EEndVar { Name = "GDP.$.G", LongName = "Темп прироста реального ВНП (Валового Национального продукта)" },
+                new EEndVar { Name = "GDP.$.IND", LongName = "Индекс реального ВНП" },
+                new EEndVar { Name = "PCI.$.IND", LongName = "Индекс реального дохода на душу населения" },
+                new EEndVar { Name = "BELT.TIGHT.IND", LongName = "Индекс поставок на внутренний рынок" },
+		new EEndVar { Name = "PUBL.INFRA.ASSETS/GDP.Q", LongName = "Отношение общей инфраструктуры к ВНП" },
+		new EEndVar { Name = "UNEMPL.Q", LongName = "Число безработных" },
+		new EEndVar { Name = "EXT.DEBT.TOT.SER/EXP.Q", LongName = "Общие выплаты по внешнему долгу по отношению к экспорту" },
+		new EEndVar { Name = "EXT.DEBT.PUBL/GDP.$.Q", LongName = "Отношение внешнего долга к ВНП" },
+                new EEndVar { Name = "IMP.SURPL.$", LongName = "Реальный излишек импорта" },
+                new EEndVar { Name = "INFLAT.R", LongName = "Уровень инфляции" },
+		new EEndVar { Name = "BUDG.DEF.TOT/GNP.Q", LongName = "Общий дефицит бюджета по отношению к ВНП" },
+		new EEndVar { Name = "BUDG.DEF.PRIM./GNP.Q", LongName = "Первичный дефицит бюджета по отношению к ВНП" },
+		new EEndVar { Name = "NAT.DEBT/GNP.Q", LongName = "Общественный долг по отношению к ВНП" },
+		new EEndVar { Name = "NAT.DEBT.SER/GNP.Q", LongName = "Выплаты по общественному долгу по отношению к ВНП" },
+                new EEndVar { Name = "GINI.Q", LongName = "Коэффициент Gini" },
+                new EEndVar { Name = "UNEMPL.INC.Q", LongName = "Отношение дохода безработных к среднему доходу" },
+		new EEndVar { Name = "UNEMPL.INC.MIN.Q", LongName = "Минимальное отношение дохода безработных к среднему доходу" },
+		new EEndVar { Name = "OFFICER.INC.Q", LongName = "Отношение доходов служащих к среднему доходу" },
+		new EEndVar { Name = "PENSIONERS.INC.Q", LongName = "Отношение дохода пенсионеров к среднему доходу" },
+		new EEndVar { Name = "SOC.EXPEND.Q", LongName = "Отношение социальных расходов бюджета к общим расходам бюджета" },
+		new EEndVar { Name = "SECUR.EXPEND.Q", LongName = "Отношение расходов бюджета на оборону к общим расходам бюджета" }
             };
 
             instrVars = new List<EInstrVar>
             {
-                new EInstrVar { Name = "Instr.Var1", LongName = "Какая то хуйня" },
-                new EInstrVar { Name = "Instr.Var2", LongName = "Какая то хуйня" },
-                new EInstrVar { Name = "Instr.Var3", LongName = "Какая то хуйня" }
+                new EInstrVar { Name = "INCR.PRIV.Q", LongName = "Увеличение доли приватизированных предприятий" },
+                new EInstrVar { Name = "INCR.MARKET.Q", LongName = "Увеличение доли рыночной экономики" },
+                new EInstrVar { Name = "IMPROV.INSTIT.Q", LongName = "Улучшение уровня образованности" },
+                new EInstrVar { Name = "INCR.RESTRUCT.Q", LongName = "Увеличение уровня реорганизации" },
+                new EInstrVar { Name = "BANKRUPTCY-Y-N", LongName = "Применение закона о банкротстве (да - нет)" },
+                new EInstrVar { Name = "DEVAL.R", LongName = "Уровень девальвации" },
+                new EInstrVar { Name = "CAP.CONTR.INTENS.Q", LongName = "Интенсивность управления выводом капитала" },
+                new EInstrVar { Name = "INTEREST.NR", LongName = "Номинальная процентная ставка" },
+                new EInstrVar { Name = "IMP.DUTY.R", LongName = "Уровень импортных пошлин" },
+		new EInstrVar { Name = "TAX.TOT.R", LongName = "Общий уровень налогов" },
+                new EInstrVar { Name = "PUBL.INV.G", Темп прироста общественных вложений" },
+                new EInstrVar { Name = "UNEMPL.INC.PC.G", LongName = "Темп прироста доходов безработных на душу населения" },
+                new EInstrVar { Name = "OFFICER.INC.PC.G", LongName = "Темп прироста доходов служащих на душу населения" },
+                new EInstrVar { Name = "PENSIONER.INC.PC.G", LongName = "Темп прироста доходов пенсионеров на душу населения" },
+                new EInstrVar { Name = "SOC.EXPEND.G", LongName = "Темп прироста социальных расходов" },
+                new EInstrVar { Name = "SECUR.EXPEND.G", LongName = "Темп прироста расходов на безопасность" }
             };
 
             interVars = new List<EInterVar>

ca7ee460b3ec7eb7c915658fdc6fcf99e88e328c
Egor <egorant1.ru@mail.ru>
2019-05-05 17:21:17 +0300

    Update Game.cs

--- GameOfEconomy/MainLogic/Game.cs
+++ GameOfEconomy/MainLogic/Game.cs
@@ -37,7 +37,7 @@ namespace GameOfEconomy.MainLogic
                 new EEndVar { Name = "End.Var1", LongName = "Какая то хуйня" },
                 new EEndVar { Name = "End.Var2", LongName = "Какая то хуйня" },
                 new EEndVar { Name = "End.Var3", LongName = "Какая то хуйня" },
-                new EEndVar { Name = "End.Var4", LongName = "Какая то хуйня3" },
+                new EEndVar { Name = "End.Var4", LongName = "Какая то хуйня4" },
             };
 
             instrVars = new List<EInstrVar>
